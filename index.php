a152095783d0d840b6b21dbe6246e90399c3a582
maxni <maxni@DESKTOP-M54EM45>
2020-12-23 14:29:18 +0200

    Laba4 finish

--- index.php
+++ index.php
@@ -1,62 +1,14 @@
+<?php declare(strict_types=1) ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laba 2</title>
-    <link media="all" rel="stylesheet" href="styles.css">
-    <link media="all" rel="stylesheet" href="calculator.css">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Istok+Web:400,400italic,700,700italic" type="text/css">
-    <style>
-      body {
-        margin: 0;
-        color: #7e7e7e;
-        font: 17px/1.412 "Istok Web", "Arial", "Helvetica", sans-serif;
-        background: #f9f9f9;
-        overflow-y: scroll;
-        min-width: 320px;
-        -webkit-text-size-adjust: 100%;
-        -ms-text-size-adjust: none;
-      }
-    </style>
+    <title>Laba 4</title>
   </head>
   <body class="home-page">
-    <div id="wrapper">
-      <header id="header">
-        <div class="panel">
-          <div id='header-container' class="container">
-            <strong class="logo"><a href="#">Laba 2</a></strong>
-            <ul>
-              <li><a href="#">Home</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="hero">
-          <div class="img">
-            <div class="s1">
-              <div class="s2" style="background: url('images/banner.jpg') no-repeat 0 0; background-size: cover; width: 100%; height: 300px; display:flex; justify-content: center; align-items: center; color: #fff;">
-                <h1>Лабораторное работа №4</h1>
-              </div>
-            </div>
-          </div>
-
-        </div>
-        <div class="navigation">
-          <nav>
-            <ul>
-              <li class="active"><a href="#">Пункт 1</a></li>
-              <li><a href="#">Пункт 2</a></li>
-              <li><a href="#">Пункт 3</a></li>
-              <li><a href="#">Пункт 4</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <div id="main">
-
-        <div id='var-7' class="container">
-          <h1 class="hide">Вариант №7</h1>
-          <div class="holder">
+  <h1>Лабораторноя работа №4</h1>
+       
             <!-- Form login -->
 
 
@@ -91,6 +43,7 @@
                   </div>
                 <?php } ?>
               <?php } ?>
+
               
               <form class="login" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 
@@ -117,11 +70,6 @@
           </div>
         </div>
       </div>
-      <footer id="footer">
-        <ul>
-          <li>© Copyright 2020 Maxim</li>
-        </ul>
-      </footer>
     </div>  
   </body>
 </html>
